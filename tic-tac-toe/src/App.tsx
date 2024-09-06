8488a406c3359e34d23ec830f54ddafb8667d6f6
Jaya Patidar <jaya.patidar@mindpathtech.com>
2024-09-06 10:15:20 +0530

    project setup

---
+++ tic-tac-toe/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Square from "./components/square";
+
+function App() {
+  return (
+    <>
+      <Square />
+    </>
+  );
+}
+
+export default App;
